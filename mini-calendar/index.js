3c5361bcbe92ae2d0ba7b49dfe081f0d02fd8482
colakalpeer <colakalpeer93@gmail.com>
2024-05-03 23:58:29 +0300

    feat: date and month added

--- mini-calendar/index.js
+++ mini-calendar/index.js
@@ -2,3 +2,9 @@ const monthNameEl = document.getElementById("month-name");
 const dayNameEl = document.getElementById("day-name");
 const dayNumEl = document.getElementById("day-number");
 const yearEl = document.getElementById("year");
+
+const date = new Date();
+const month = date.getMonth();
+monthNameEl.innerText = date.toLocaleString("en", {
+  month: "long",
+});
