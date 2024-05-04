98134a15c4777f38d6a8298ffeb322374fbda255
colakalpeer <colakalpeer93@gmail.com>
2024-05-04 23:24:54 +0300

    feat: refactor date and month

--- mini-calendar/index.js
+++ mini-calendar/index.js
@@ -8,3 +8,11 @@ const month = date.getMonth();
 monthNameEl.innerText = date.toLocaleString("en", {
   month: "long",
 });
+
+dayNameEl.innerText = date.toLocaleString("en", {
+  weekday: "long",
+});
+
+dayNumEl.innerText = date.getDate();
+
+yearEl.innerText = date.getFullYear();
